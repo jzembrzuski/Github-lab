f648ca6ef24acab48012adfba6ed04fbf987f60d
jzembrzuski <jzembrzuski@gmail.com>
2016-09-27 10:15:17 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,27 @@
 
+/*
+
+ * Program to print name
+
+ * @author Jake Zembrzuski
+
+ */
+
+import java.util.Scanner; 
+
+
+
+public class hello {
+
+
+    /**
+
+     * @param args
+
+     */
+
+    public static void main(String[] args) {
+      
+        System.out.println("Hello World.");
+    }
+}
